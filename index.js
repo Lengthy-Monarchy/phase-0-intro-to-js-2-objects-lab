b2402b600298ea891690e63d468a72bdffb903f8
anne <anneirungu07@gmail.com>
2024-01-09 20:08:25 +0300

    commited assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+    name: 'John Doe',
+    streetAddress: '123 Main St',
+};
+
+// Function to update an employee with a new key-value pair (non-destructive)
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+    ...employee,
+    [key]: value,
+};
+}
+
+// Function to update an employee with a new key-value pair (destructive)
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+//Function to delete a key
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+// Function to delete a key  (destructive)
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+
